1a564d84cb116108624b93e61fcedc52d371799d
HaiAnh-1404 <hainguyenanh00@gmail.com>
2026-06-09 20:59:20 +0700

    update  UI and link fanpage

---
+++ SweetShellCup/Pages/Support/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SweetShellCup.Interfaces;
+using System.Security.Claims;
+
+namespace SweetShellCup.Pages.Support
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ICartRepository _cart;
+
+        public IndexModel(ICartRepository cart) { _cart = cart; }
+
+        public async Task OnGetAsync()
+        {
+            ViewData["Title"] = "Hỗ trợ khách hàng";
+            ViewData["ActivePage"] = "Support";
+
+            int cartCount = 0;
+            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (int.TryParse(userIdString, out var userId))
+            {
+                cartCount = await _cart.GetCartItemCountAsync(userId);
+            }
+            ViewData["CartCount"] = cartCount;
+        }
+    }
+}
